f271139f3bdef5933034cc95b794392664176a51
iamjayenmthakuri <malla.jayen@gmail.com>
2023-03-28 15:02:04 +0545

    committing second time

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -5,9 +5,8 @@ from .models import ToDoList,Item
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
-    item = ls.item_set.get(id=1)
-    return HttpResponse("<h1>%s<h1><br></br><p>%s</p>" %(ls.name, str(item.text)))
+    return render(response, "main/base.html", {"name": ls.name})
 
 
 def home(response):
-    pass
+    return render( response, "main/home.html", {"name":"test"})
